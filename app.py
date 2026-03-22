048ae4d7a5170d499ed03c62330f3bd21a7d8a0b
Sneha-Kansara <snehakansara28@gmail.com>
2026-03-22 19:13:02 +0530

    Refactor agent setup and prompt template usage

--- app.py
+++ app.py
@@ -85,36 +85,22 @@ else:
         ]
 
         # 3. Setup Agent
-       from langchain_core.prompts import PromptTemplate
-        prompt_template = '''Answer the following questions as best you can. You have access to the following tools:
+ from langchain_core.prompts import PromptTemplate
 
-{tools}
+    prompt_template = """Answer the following questions...
+    """
 
-Use the following format:
+    prompt = PromptTemplate.from_template(prompt_template)
 
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
+    agent = create_react_agent(llm, tools, prompt)
 
-Begin!
-
-Question: {input}
-Thought:{agent_scratchpad}'''
-        prompt = PromptTemplate.from_template(prompt_template)
-        
-        agent = create_react_agent(llm, tools, prompt)
-        agent_executor = AgentExecutor(
-            agent=agent, 
-            tools=tools, 
-            verbose=True, 
-            handle_parsing_errors=True,
-            max_iterations=5  # Prevent runaway loops that waste quota
-        )
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        verbose=True,
+        handle_parsing_errors=True,
+        max_iterations=5
+    )
     except Exception as e:
         st.error(f"Initialization Error: {e}")
 
